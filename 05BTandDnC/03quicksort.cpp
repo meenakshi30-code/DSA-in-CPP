aa449c88668dc89b7bb1a4cac662d04a595b2cb3
Meenakshi Paul <meenakshipaul379@gmail.com>
2026-01-15 20:14:53 +0530

    Added quick sort implementation

---
+++ 05BTandDnC/03quicksort.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+  public:
+  
+    int partition(int a[], int low, int high) {
+        int pivot = a[high];
+        int i = low - 1;
+
+        for(int j = low; j < high; j++) {
+            if(a[j] < pivot) {
+                i++;
+                swap(a[i], a[j]);
+            }
+        }
+        swap(a[i + 1], a[high]);
+        return i + 1;
+    }
+
+    void quickSort(int a[], int low, int high) {
+        if(low < high) {
+            int p = partition(a, low, high);
+            quickSort(a, low, p - 1);
+            quickSort(a, p + 1, high);
+        }
+    }
+};
+
+int main() {
+    Solution s;
+    int arr[] = {10, 7, 8, 9, 1, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    s.quickSort(arr, 0, n - 1);
+
+    cout << "Sorted array: ";
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
